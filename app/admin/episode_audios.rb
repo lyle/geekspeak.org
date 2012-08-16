dab663c7d91f452d7c95231dde668be166ca5cfb
Lyle Troxell <lyle@troxell.com>
2012-08-16 20:12:27 +0000

    Removed Audio Managment from ActiveAdmin - started displaying bits in segments in Active Admin

--- app/admin/episode_audios.rb
+++
@@ -1,3 +0,0 @@
-ActiveAdmin.register EpisodeAudio do
-  
-end
--- app/admin/segments.rb
+++ app/admin/segments.rb
@@ -17,7 +17,22 @@ ActiveAdmin.register Segment do
 
       f.buttons
    end
-   
+  show do 
+    h1 "Segment: #{segment.title}"
+    h2 "#{segment.episode.showdate_as_url} : #{segment.episode.title}" 
+    panel "Bits" do
+      table_for segment.bits do
+        column :title do |bit|
+          link_to bit.title, bit.url
+        end
+        column :body
+        column :user
+        column :edit do |bit|
+          link_to "edit", edit_admin_bit_path(bit)
+        end
+      end
+    end   
+  end   
    
    collection_action :sort, :method => :post do
        params[:segment].each_with_index do |id, index|
