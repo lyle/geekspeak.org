4cda565ad5f850541d95b047458090aefb9df07f
Lyle Troxell <lyle@troxell.com>
2012-08-16 12:21:23 -0700

    Allow setting of passwords for users in Active Admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -7,5 +7,23 @@ ActiveAdmin.register User do
       column :last_sign_in_at
       column :sign_in_count
       default_actions
+  end
+  controller do
+
+  end
+  show do 
+    h1 user.login
+    h2 user.display_name
+    active_admin_comments  
+  end
+  form do |f|
+    f.inputs "Details" do
+      f.input :login
+      f.input :display_name
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
     end
+    f.buttons
+  end
 end
