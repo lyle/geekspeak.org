6daba1a9ee94a8a055170eb70430254eef58a492
Lyle Troxell <lyle@troxell.com>
2012-08-22 00:37:45 -0700

    rebase messed me up… not sure what is going on with GitHub app

--- app/models/bit.rb
+++ app/models/bit.rb
@@ -1,11 +1,14 @@
 class Bit < ActiveRecord::Base
   has_many :segment_bits
   has_many :segments, :through => :segment_bits   
-  
   has_many :bit_episodes
   has_many :episodes, :through => :bit_episodes
+<<<<<<< HEAD
   acts_as_list :scope => :episoe
 
+=======
+  acts_as_list :scope => :episode
+>>>>>>> Got /bits working limited, and moved _bits to bit dir
   belongs_to :user
   attr_accessible :title, :url, :user_id, :body, :status
   
