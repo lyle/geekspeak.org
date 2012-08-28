c2be3fd67d92de7804ee1cb01a8bda27b4c36a18
Lyle Troxell <lyle@troxell.com>
2012-08-28 11:52:22 -0700

    oh, and precompile the admin.css too... err

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,8 @@ BetaGeekspeakOrg::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( admin.js )
 
+  config.assets.precompile += %w( admin.css )
+
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
