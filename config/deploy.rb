b05b844c1fcf79b3dc081e3299d7050a20f68d79
Lyle Troxell <lyle@troxell.com>
2012-08-28 12:56:16 -0700

    precomiple assests conditionally - fix upload linking

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,10 +23,26 @@ set :branch, "master"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
-task :link_upload_folders do
-    run "ln -nfs #{shared_path}/shows/audio #{release_path}/public/shows/audio"
-    run "ln -nfs #{shared_path}/episodes #{release_path}/public/episodes"
-end
+
+namespace :deploy do
+     namespace :assets do
+       task :precompile, :roles => :web, :except => { :no_release => true } do
+         from = source.next_revision(current_revision)
+         if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+           run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+         else
+           logger.info "Skipping asset pre-compilation because there were no asset changes"
+         end
+     end
+     
+     task :link_upload_folders do
+         run "ln -nfs #{shared_path}/shows/audio #{release_path}/public/shows/audio"
+         run "ln -nfs #{shared_path}/episodes #{release_path}/public/episodes"
+     end
+   end
+ end
+
+
 
 
 after "deploy:symlink","deploy:link_upload_folders"
