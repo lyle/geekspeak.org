e23c003c3d5a411bda55f2b343ee1ca73cc7ddfb
Lyle Troxell <lyle@troxell.com>
2012-08-31 01:56:12 -0700

    fixed :url for episode images

--- app/models/episode_image.rb
+++ app/models/episode_image.rb
@@ -3,6 +3,6 @@ class EpisodeImage < ActiveRecord::Base
   attr_accessible :image
   has_attached_file :image, :styles => {:large=> "800x800", :medium => "600x600>", :small => "300x600", :smaller=> "200x200", :thumb => "100x100>" },
                     :path => ":rails_root/../../shared/episodes/:episode_showdate_as_url/:basename-:style.:extension",
-                    :url => "/episodes/:episode_showdate_as_url/teaser-:style.:extension"
+                    :url => "/episodes/:episode_showdate_as_url/:basename-:style.:extension"
   
 end
