db81b69e01e1c91396c3a0363d3d53ce1fd99ee5
Lyle Troxell <lyle@troxell.com>
2013-06-01 17:02:14 -0700

    keep btn-group broken

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,22 +30,22 @@ $(function() {
 	});
 
 
-//$('div.btn-group[data-toggle-name=*]').each(function(){
-//    var group   = $(this);
-//    var form    = group.parents('form').eq(0);
-//    var name    = group.attr('data-toggle-name');
-//    var hidden  = $('input[name="' + name + '"]', form);
-//    $('button', group).each(function(){
-//      var button = $(this);
-//      button.live('click', function(){
-//		$(this).parent().children().removeClass('active');
-//        hidden.val($(this).val());
-//		hidden.trigger('change');
-//		$(this).addClass('active')
-//      });
-//      if(button.val() == hidden.val()) {
-//        button.addClass('active');
-//      }
-//    });
-//  });
+$('div.btn-group[data-toggle-name=*]').each(function(){
+    var group   = $(this);
+    var form    = group.parents('form').eq(0);
+    var name    = group.attr('data-toggle-name');
+    var hidden  = $('input[name="' + name + '"]', form);
+    $('button', group).each(function(){
+      var button = $(this);
+      button.live('click', function(){
+		$(this).parent().children().removeClass('active');
+        hidden.val($(this).val());
+		hidden.trigger('change');
+		$(this).addClass('active')
+      });
+      if(button.val() == hidden.val()) {
+        button.addClass('active');
+      }
+    });
+  });
 });
