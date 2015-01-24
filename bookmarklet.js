a53c7d66b46fd8877dff4053b66735e89f2655ce
Lyle Troxell <lyle@troxell.com>
2015-01-23 16:35:41 -0800

    Un-minified bookmarklet url

---
+++ bookmarklet.js
@@ -0,0 +1,10 @@
+javascript:location.href=
+    "http://localhost:3000/bits/new/?return_url="+
+    encodeURIComponent(document.URL)+
+    "&bit[url]="+
+    encodeURIComponent(document.URL)+
+    "&bit[title]="+
+    encodeURIComponent(document.title)+
+    "&bit[body]=bq. "+
+    encodeURIComponent(document.getSelection());
+
