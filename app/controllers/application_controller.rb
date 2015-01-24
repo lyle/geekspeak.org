db4a5cbed89e59d4e964d2107adcfd8a2e7591a2
Lyle Troxell <lyle@troxell.com>
2015-01-24 01:31:13 -0800

    fix login and logout redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,5 +16,13 @@ class ApplicationController < ActionController::Base
     return nil if user_signed_in? && !current_user.admin? 
     current_user
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    episodes_path
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    episodes_path
+  end
   
 end
