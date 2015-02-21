1652135b9a1640fe57be559d66f800ba5b6e890c
Lyle Troxell <lyle@troxell.com>
2015-02-21 11:37:00 -0800

    Remove X for remove of participants if not logged in

--- app/controllers/bits_episode_controller.rb
+++ app/controllers/bits_episode_controller.rb
@@ -1,6 +1,6 @@
 class BitsEpisodeController < ApplicationController
   before_filter :authenticate_user!
-  
+
   respond_to :html, :json
   def sort
     params[:bits_episode].each_with_index do |id, index|
