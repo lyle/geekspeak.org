f1ec423ed7f0dc4e5d86a0e559479d106da4361e
Lyle Troxell <lyle@troxell.com>
2015-09-29 12:59:23 -0700

    Disable NPR from Getting New shows

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,7 +2,7 @@ class FeedsController < ApplicationController
     
     def nprepisodes
         #npr is using this
-        @episodes = Episode.joins(:episode_audios).where(:status => 'live').order('airdate DESC').limit(10)
+        @episodes = Episode.joins(:episode_audios).where('airdate < ?', "2015/09/28".to_date).where(:status => 'live').order('airdate DESC').limit(10)
     end
     def episodes
         @episodes = Episode.joins(:episode_audios).where(:status => 'live').order('airdate DESC').limit(10)
