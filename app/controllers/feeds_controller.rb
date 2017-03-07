074bf7fb9b539e339b61c279c435ddaf609b479f
Lyle Troxell <lyle@troxell.com>
2017-03-07 07:41:18 -0800

    Reduce Episode list in RSS to 10 for speed improvements

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -5,7 +5,7 @@ class FeedsController < ApplicationController
         @episodes = Episode.joins(:episode_audios).where('airdate < ?', "2015/09/28".to_date).where(:status => 'live').order('airdate DESC').limit(10)
     end
     def episodes
-        @episodes = Episode.joins(:episode_audios).where(:status => 'live').where(['publication_time < ? OR publication_time IS NULL', DateTime.now]).order('airdate DESC').limit(60)
+        @episodes = Episode.joins(:episode_audios).where(:status => 'live').where(['publication_time < ? OR publication_time IS NULL', DateTime.now]).order('airdate DESC').limit(10)
     end
     def sitemap
         @episodes = Episode.where(:status => 'live').where(['publication_time < ? OR publication_time IS NULL', DateTime.now]).order('airdate DESC')
