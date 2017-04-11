fa107cb27988c00507fc39d8ac8bc5516abbcf27
Lyle Troxell <lyle@troxell.com>
2017-04-11 13:48:40 -0700

    Make the homepage hide live but not published shows

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
     before_filter :authenticate_user!, :except => [:index, :show]
   def index
-    @episodes = Episode.where(:status=>'live').limit(8).order('airdate DESC').includes(:participants)
+    @episodes = Episode.where(:status=>'live').where(['publication_time < ? OR publication_time IS NULL', DateTime.now]).limit(8).order('airdate DESC').includes(:participants)
   end
 end
