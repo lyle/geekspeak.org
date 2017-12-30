b8d4eee40322c7c9209aa9b558ce9254a3f74c22
Lyle Troxell <lyle@troxell.com>
2017-12-29 23:25:44 -0800

    making sure puma starts from bundler exec app context

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ set :rbenv_ruby, File.read('.ruby-version').strip
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
-
+append :rbenv_map_bins, 'puma', 'pumactl'
 
 ## Defaults:
 # set :scm,           :git
